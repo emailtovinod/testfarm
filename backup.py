ebaf4df4d79eeb7bd75a4f3395c800ed681eff4b
anudeepgupta42 <33155761+anudeepgupta42@users.noreply.github.com>
2018-03-01 18:45:54 +0530

    back up 01-mar-2018 6:45

--- backup.py
+++ backup.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 27 12:41:20 2018
-
-@author: anumula_anudeep
-"""
-
-# -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request
 import json,apiai, requests
@@ -41,16 +34,11 @@ def login():
 @application.route("/get")
 def get_bot_response():
     global glb_username
-    
+    admin_email= 'admin@example.com'
+    priority_lst = {'low': 1 ,'medium': 2 , 'high':3,  'urgent': 4}
     """ Fetching user dialog from UI """
     userText = request.args.get('msg')
-    
-    """ the below connection to dialog flow canbe replaced with Dialogflow_connection()  """ 
-    """ CLIENT_ACCESS_TOKEN = '339d47f35f864a849a6dd17b6e3ede46'
-    ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
-    airequest = ai.text_request()
-    airequest.lang = 'de'  # optional, default value equal 'en'
-    airequest.session_id = "<SESSION ID, UNIQUE FOR EACH USER>" """
+
     airequest = Dialogflow_connection() 
     
     airequest.query = userText
@@ -63,19 +51,78 @@ def get_bot_response():
     
     reply = obj['result']['fulfillment']['speech']
     ticket_type=str(obj['result']['action'])
-
-    print(ticket_type)
+    action = ticket_type
+    print(action)
     print(str(obj['result']['parameters']))
     
-    if ticket_type=='L0':
+    if action=='show':
+        
+        print("In show ticket")
+        ticket_id=str(obj['result']['parameters']['ticketid'])
+        display =str(obj['result']['parameters']['display'])
+        print(ticket_id)
+        print(display)
+        
+        if (ticket_id != '' and display != ''):
+            
+            res = show_ticket(ticket_id)
+            if res.status_code == 200:
+                print ("Request processed successfully, the response is given below")
+                priority_map = {1:'low', 2:'medium', 3:'high', 4:'urgent'}
+               
+                L1 = "please find below the ticket details"
+                L2 = " <br /> <b>Ticket Id</b>: " + str(res.json()['id'])
+                L3 = " <br /> <b>Requestor Email</b>: " + res.json()['requester']['email']
+                L4 = " <br /> <b>Ticket Type</b>: " + res.json()['type']
+                L5 = " <br /> <b>ticket Description</b>: " + res.json()['description_text']
+                
+                prioroty_disp = priority_map[res.json()['priority']]
+                L6 = " <br /> <b>priority</b>: " + str(prioroty_disp)
+
+                reply = L1 + L2 + L6 + L3 + L4 + L5 
+
+            else:
+                    reply = "Failed to fetch ticket details, please check with a valid ticket id"
+
+        obj = ''
+            
+    
+
+    if action=='update':
+        
+        print("In update ticket")
+        ticket_id=str(obj['result']['parameters']['ticketid'])
+        tkt_priority=str(obj['result']['parameters']['priority'][0])
+       
+
+        update=str(obj['result']['parameters']['update'])
+        
+        reply = obj['result']['fulfillment']['speech']
+
+        print(ticket_id)
+        
+        if (ticket_id != '' and tkt_priority != '' and update != ''):
+            
+            tkt_priority = priority_lst[tkt_priority.lower()]
+            res = update_ticket(ticket_id, tkt_priority)
+
+            if res.status_code != 200:
+                reply = "Failed to update ticket, please check with a valid ticket id and priority"
+
+        obj = ''
+            
+    
+    if action=='L0':
         
         """ call to QNA forgetting replies from FAQ pages """
         query = str(obj['result']['resolvedQuery'])
+        tkt_priority = 1
+        tkt_status = 4
         resp_from_QNA = QNA(query)
         Deflt_resp = "No good match found in the KB"
         
         if resp_from_QNA.lower() == Deflt_resp.lower() :
-            reply = "Sorry, I am not trained for this"
+            resp_from_QNA = "Sorry, I am not trained for this"
         
         """ Establishing connection with Agentted.db """
         con = create_connection()
@@ -86,18 +133,21 @@ def get_bot_response():
         con.commit()
         
         con.close()
-        
+        ticket_id = log_ticket(ticket_type, query, admin_email, tkt_priority, tkt_status)
         reply = str(resp_from_QNA)
         obj = ''
         print('L0 ticket lodged')
     
-    if ticket_type=='L1':
+    if action=='L1':
         status = ''
         status_updt = 'RUNNING'
         """  Extracting Entities from return obj of dialogflow """ 
         print(str(obj['result']['parameters']['Vmname']))
         Vmname=str(obj['result']['parameters']['Vmname'])
         action=str(obj['result']['parameters']['action'])
+        tkt_priority = 1
+        tkt_status = 4
+        
         print(Vmname)
         query = action + Vmname
         
@@ -124,9 +174,11 @@ def get_bot_response():
             action = action.lower()
             
             print(action)
-            stop_lst= ['stop' , 'shutdown' , 'shut down']   
-            start_lst = ['start', 'run']
+            stop_lst= ['stop' , 'shutdown' , 'shut down']
+            start_lst = ['start' , 'run']
             terminate_list = ['terminate']
+            
+            restart_list = ['restart' , 'reboot']
 
            
             if (status == ''):
@@ -147,7 +199,7 @@ def get_bot_response():
                 print(reply)
                 obj = ''
 	        
-            elif (((action in  start_lst) or (action in  stop_lst) or (action in  terminate_list))and (status == 'terminated')):
+            elif (((action in  start_lst) or (action in  stop_lst) or (action in  terminate_list) or (action in  restart_list)) and (status == 'terminated')):
                 
                 reply = Vmname + ' is already terminated. No further operations can be performed.'
                 print(reply)
@@ -183,12 +235,12 @@ def get_bot_response():
                     print(max_id)
                 con.close()
                 reply = "Ticket: " + str(max_id) + " " + str(reply)
-            
-                reply = Vmname + " is sucessfully " + action + "ed. ticket_id: " + str(max_id) 
+                ticket_id = log_ticket(ticket_type, query, admin_email, tkt_priority, tkt_status)
+                reply = Vmname + " is sucessfully " + action + "ed. ticket_id: " + str(ticket_id) 
                 obj = ''
                 print('L1 ticket lodged')
 
-    if ticket_type=='L11':
+    if action=='L11':
         
         """  Extracting Entities from return obj of dialogflow """
         ticket_type = 'L1'
@@ -197,6 +249,8 @@ def get_bot_response():
         vmSize=str(obj['result']['parameters']['vmSize'])
         action='provision'
         #print(obj)
+        tkt_priority = 1
+        tkt_status = 4
         query = 'provision ' + vmSize +" instance in " + Env
         print(query)
         """  Validating if all the required entities for L1 are extracted from user dialogs """ 
@@ -228,7 +282,7 @@ def get_bot_response():
                 ## adding new entry to the entity vmname
                 dialogflow_entity(vmname)
                 SUBSCRIPTION_ID = 'xxxx-xxxx-xxxx-xxxx' 
-                VM_STATUS = 'running'
+                VM_STATUS = 'RUNNING'
                 """  Logging ticket for L1 """ 
                 cur.execute("INSERT INTO TICKETS (NAME,CATEGORY,QUERY, ACTION, VM_SIZE, ENV, vmname) VALUES (?,?,?,?,?,?,?)",(glb_username,ticket_type, query, action, vmSize, Env, vmname) )
                 con.commit()
@@ -244,6 +298,8 @@ def get_bot_response():
                     print(row)
                     ticket_id = row[0]
                 
+                ticket_id = log_ticket(ticket_type, query, admin_email, tkt_priority, tkt_status)
+                
                            
                 con.close()
                 
@@ -256,16 +312,20 @@ def get_bot_response():
                 obj = ''
                 print('L11 ticket lodged')
                        
-    if ticket_type=='L2':
+    if action=='L2':
         
         """   Extracting Entities from return obj of dialogflow """ 
         Vmname=str(obj['result']['parameters']['Vmname'])
         user=str(obj['result']['parameters']['user'])
         description=str(obj['result']['parameters']['description'])
-        query = description + Vmname
+        tkt_priority = str(obj['result']['parameters']['priority'])
+        
+        
+        query =  Vmname + ' ' + description
+        tkt_status = 2
        
         """  Validating if all the required entities for L2 are extracted from user dialogs """ 
-        if (Vmname != '' and user != '' and description != ''):
+        if (Vmname != '' and user != '' and description != '' and tkt_priority != ''):
            
             """   Establishing connection with Agentted.db """ 
             con = create_connection()
@@ -285,12 +345,13 @@ def get_bot_response():
                 print(max_id)
            
             con.close()
-            reply = "Ticket: " + str(max_id) + " " +str(reply)
+            tkt_priority = priority_lst[tkt_priority.lower()]
+            ticket_id = log_ticket(ticket_type, query, admin_email, tkt_priority, tkt_status)
+            reply = "Ticket: " + str(ticket_id) + " " +str(reply)
             obj = ''
             
     return str(reply)
-
-
+    
     
 @application.route('/foo', methods=['POST','GET'])
 def tickets():
@@ -326,18 +387,7 @@ def table_creation():
     conn.execute("INSERT INTO TICKETS (NAME, QUERY,CATEGORY) VALUES (?,?,?)",(name, query,tkt_type) )
     conn.commit()
     conn.execute('CREATE TABLE IF NOT EXISTS VM_INSTANCE (USER_NAME CHAR(100), SUBSCRIPTION_ID CHAR(100), VM_NAME CHAR(100), VM_STATUS CHAR(100))')
-    """
-    cur = conn.cursor()
-    cur.execute("select * from TICKETS")
-    print('TICKETS')
-    names_tickets = list(map(lambda x: x[0], cur.description))
-    print(names_tickets)
-    
-    cur.execute("select * from VM_INSTANCE")
-    print('VM_INSTANCE')
-    names_instances = list(map(lambda x: x[0], cur.description))
-    print(names_instances)
-    """
+
     conn.close()
     print("Tables created")
 
@@ -356,7 +406,6 @@ def QNA(qsn):
         'Content-Type': 'application/json',
         'Ocp-Apim-Subscription-Key': '6b87248960c1422da579715d1ffd818a',
         }
-    #qsn='Does Azure support IPv6?'
     body = "{'question':'" + qsn + "'}"
 
     params = urllib.parse.urlencode({
@@ -389,32 +438,106 @@ def dialogflow_entity(vmname):
     print(response)
     print(response.json)
     
+def log_ticket(ticket_type, query, admin_email, priority, status):
+	api_key = "1d3OInmn7R770ovj0ZZI"
+	domain = "infosysanudeep"
+	password = "x"
+	ticket_id = ""
+
+
+	headers = { 'Content-Type' : 'application/json' }
+
+	ticket = {
+		'subject' : ticket_type,
+		'description' : query,
+		'email' : admin_email,
+		'type': ticket_type,
+		'priority' : priority,
+		'status' : status,
+            	'source' : 7,
+	}
+
+	r = requests.post("https://"+ domain +".freshdesk.com/api/v2/tickets", auth = (api_key, password), headers = headers, data = json.dumps(ticket))
+
+	if r.status_code == 201:
+	  print ("Ticket created successfully, the response is given below")
+	  ticket_id = r.json()['id']
+	  print ("Location Header : " + r.headers['Location'])
+	  return ticket_id
+	else:
+	  print ("Failed to create ticket, errors are displayed below,")
+	  response = r.json()
+	  print (response["errors"])
+
+	  print ("x-request-id : " + r.headers['x-request-id'])
+	  print ("Status Code : " + str(r.status_code))
+	  return ticket_id
+
+def show_ticket(ticket_id):
+    api_key = "1d3OInmn7R770ovj0ZZI"
+    domain = "infosysanudeep"
+    password = "x"
+
+	# Id of the ticket to be updated
+	#ticket_id = '12'
+    res = requests.get("https://"+ domain +".freshdesk.com/api/v2/tickets/"+ ticket_id +"?include=requester", auth = (api_key, password))
+    return res
+
+def update_ticket(ticket_id,priority):
+	api_key = "1d3OInmn7R770ovj0ZZI"
+	domain = "infosysanudeep"
+	password = "x"
+
+	headers = { 'Content-Type' : 'application/json' }
+
+	ticket = {
+	  'priority' : int(priority),
+	}
+
+	res = requests.put("https://"+ domain +".freshdesk.com/api/v2/tickets/"+ticket_id, auth = (api_key, password), headers = headers, data = json.dumps(ticket))
+	return res
 
 
 @application.route('/dashboard', methods=['POST','GET'])
 def dashboard():
       con = create_connection()
+      con.row_factory = sqlite3.Row
+      cur = con.cursor()
+      cur.execute("select * from VM_INSTANCE")
+      rows = cur.fetchall()
+      print(rows)
       dataframe = pd.read_sql_query("select * from TICKETS", con)
-      print(dataframe)
+
       print(dataframe['CATEGORY'])
       print(dataframe['CATEGORY'].value_counts())
-#           print(dataframe['CATEGORY'])
-#           sns.distplot(dataframe['CATEGORY'].value_counts())
-      #           plt.bar(dataframe['USER'],dataframe['CATEGORY'])
+
       fig = plt.figure()    
       dataframe.groupby('CATEGORY').size().plot(kind='bar')
       path='static/media/graph.png'
-      
-#      plt.bar(dataframe['CATEGORY'],dataframe['CATEGORY'].value_counts())
-#      plt.xlabel("Ticket Category")
-#      plt.ylabel("Number of Tickets") 
-#      path='static/media/graph.png'
+
       print(path)
       fig.savefig(path)
-#           image_to_render = base64.b64encode(path).decode('ascii')
-#           return send_plots(path)
+
       print("path ",path)
-      return render_template("dashboard.html",path=path)
-          
+      return render_template("dashboard.html",path=path,rows=rows)
+  
+#def dashboard():
+#      con = create_connection()
+#      dataframe = pd.read_sql_query("select * from TICKETS", con)
+##      print(dataframe)
+##      print(dataframe['CATEGORY'])
+##      print(dataframe['CATEGORY'].value_counts())
+#
+#      fig = plt.figure()    
+#      dataframe.groupby('CATEGORY').size().plot(kind='bar')
+#      path='static/media/graph.png'
+#   
+#      fig.savefig(path)
+#
+#      print("path ",path)
+#      return render_template("dashboard.html",path=path)
+#          
+
+
 if __name__ == "__main__":
     main()
